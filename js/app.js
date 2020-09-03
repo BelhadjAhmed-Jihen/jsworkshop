0497a8ea575b744dd6a18749c75a3dfe41e9de33
Ferid HELALI <helaliferid@gmail.com>
2020-09-03 15:30:36 +0200

    :)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
     const output=document.querySelector('#output');
     output.innerHTML="We are the World 1984";
+    setTimeout(function(){
+        output.innerHTML=":)";
+    },5000)
 });
